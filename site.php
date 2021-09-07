7b6ff53f44eead7b138841370b93bdc1431c3a8d
Dameon <shubhamsingh0602@gmail.com>
2021-09-07 07:39:53 +0530

    Day1

---
+++ site.php
@@ -0,0 +1,29 @@
+<!-- PHP is just like JSX of Java -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        // echo("Hello World");
+        // echo "<h1>Dameon's Site</h1>";
+        // echo("<hr>");
+        // echo "<p> This is my site. </p";
+
+        $character_name = "Dameon";
+        $character_age = "35";
+
+        echo("There once was a man name named $character_name. <br>"); // Similar to react
+        echo("He was $character_age years old. <br>");
+        echo("He really liked the name $character_name. <br>");
+        echo("But he didn't like being $character_age. <br>");
+    ?>
+
+</body>
+</html>
