49bb02562984738349d8ca2ef7172fbaabb1cbd7
Dameon <shubhamsingh0602@gmail.com>
2021-09-12 05:50:44 +0530

    Day2

--- site.php
+++ site.php
@@ -23,6 +23,54 @@
         // echo("He was $character_age years old. <br>");
         // echo("He really liked the name $character_name. <br>");
         // echo("But he didn't like being $character_age. <br>");
+
+        // DATA TYPES
+        // $phrase = "To be or not to be";
+        // $age = 23;
+        // $gpa = 7.43;
+        // $isMale = true;
+        // null;
+        // echo $phrase;
+
+        // STRINGS
+        // $phrase = "Giraffe Academy";
+        // echo $phrase;
+        // echo strtolower($phrase);
+        // echo strtoupper($phrase);
+        // echo strlen($phrase);
+        // echo $phrase[0];
+        // $phrase[0] = "B";
+        // echo $phrase;
+        // echo str_replace("ffe", "Panda", $phrase);
+        // echo substr($phrase, 8);
+        // echo substr($phrase, 8, 3);
+
+        // NUMBERS
+        // echo 5;
+        // echo -7;
+        // echo 6.7;
+        // echo 5 + 9;
+        // echo 5 - 9;
+        // echo 9 / 5;
+        // echo 5 * 9;
+        // echo 10 % 3;
+        // echo 4 + 5 * 10;
+        // $num = 10;
+        // echo $num;
+        // $num++;
+        // echo $num;
+        // $num--;
+        // echo $num;
+        // $num += 25;
+        // echo $num;
+        // echo abs(-100);
+        // echo pow(2,4);
+        // echo sqrt(36);
+        // echo max(2,5);
+        // echo min(2,5);
+        // echo round(3.2);
+        // echo ceil(3.3);
+        // echo floor(3.3);
     ?>
 
 </body>
